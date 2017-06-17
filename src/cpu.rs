593691ecd5cbd2931c50fd4f3a6bc81cdc6e8d42
wwared <wwared@users.noreply.github.com>
2017-06-16 23:56:05 -0300

    Finish main opcode decoding

--- src/cpu.rs
+++ src/cpu.rs
@@ -115,11 +115,12 @@ pub enum Instruction {
   HiWriteReg(),
 
   AddHL(Reg16),
+  LoadSPOffset(i8),
   SwapSPHL(),
 
   Pop(Reg16),
   Push(Reg16),
-  AddSp(i8),
+  AddSP(i8),
 
   Rlca(),
   Rla(),
@@ -140,7 +141,7 @@ pub enum Instruction {
   ComplementCarry(),
 
   Daa(),
-  SetCaryFlag(),
+  SetCarryFlag(),
 
   Restart(u8),
 }
@@ -155,7 +156,7 @@ impl Instruction {
         WriteAImm16(_) | LoadImm16(_, _) => 3,
       Jr(_) | JrFlag(_, _, _) | HiLoad(_) | HiWrite(_) |
         AddImm8(_) | SubImm8(_) | AndImm8(_) | OrImm8(_) |
-        LoadImm8(_, _) | AddSp(_) | AddCarryImm8(_) |
+        LoadImm8(_, _) | AddSP(_) | LoadSPOffset(_) | AddCarryImm8(_) |
         SubCarryImm8(_) | XorImm8(_) | CompareImm8(_) |
         Rlc(_) | Rrc(_) | Rl(_) | Rr(_) | Sla(_) | Sra(_) |
         Swap(_) | Srl(_) | TestBit(_, _) | SetBit(_, _, _) => 2,
@@ -166,31 +167,6 @@ impl Instruction {
 
 // TODO: unused
 //const CLOCK_FREQ_PER_SEC: u32 = 4194304;
-
-// TODO: change this
-  // note: returns number of cycles the operation took
-  // possibly change this in the future with more useful info (what?)
-// static ref OPCODES: HashMap<u8, fn(&mut CpuState) -> usize> = {
-//   let mut m = HashMap::<u8, fn(&mut CpuState) -> usize>::new();
-//   m.insert(0x00, CpuState::nop);
-//   m.insert(0x02, CpuState::ld_b_d8);
-//   m.insert(0x12, CpuState::ld_d_d8);
-//   m.insert(0x22, CpuState::ld_h_d8);
-//   m.insert(0x32, CpuState::ld_hl_d8);
-//   m.insert(0x0E, CpuState::ld_c_d8);
-//   m.insert(0x1E, CpuState::ld_e_d8);
-//   m.insert(0x2E, CpuState::ld_l_d8);
-//   m.insert(0x3E, CpuState::ld_a_d8);
-//   m.insert(0xC5, CpuState::push_bc);
-//   m.insert(0xD5, CpuState::push_de);
-//   m.insert(0xE5, CpuState::push_hl);
-//   m.insert(0xF5, CpuState::push_af);
-//   m.insert(0xC1, CpuState::pop_bc);
-//   m.insert(0xD1, CpuState::pop_de);
-//   m.insert(0xE1, CpuState::pop_hl);
-//   m.insert(0xF1, CpuState::pop_af);
-//   m
-// };
 
 fn concat_u8(h: u8, l: u8) -> u16 {
   ((h as u16) << 8) | (l as u16)
@@ -371,15 +347,37 @@ impl CPU {
       0xD0 => RetFlag(Flag::Carry, false),
       0xD8 => RetFlag(Flag::Carry, true),
 
+      0xC9 => Ret(),
+      0xD9 => RetInterrupt(),
+
       0xC2 => JpFlag(self.opcode_u16(), Flag::Zero, false),
       0xCA => JpFlag(self.opcode_u16(), Flag::Zero, true),
       0xD2 => JpFlag(self.opcode_u16(), Flag::Carry, false),
       0xDA => JpFlag(self.opcode_u16(), Flag::Carry, true),
 
+      0xC4 => CallFlag(self.opcode_u16(), Flag::Zero, false),
+      0xCC => CallFlag(self.opcode_u16(), Flag::Zero, true),
+      0xD4 => CallFlag(self.opcode_u16(), Flag::Carry, false),
+      0xDC => CallFlag(self.opcode_u16(), Flag::Carry, true),
+
+      0xCD => Call(self.opcode_u16()),
+
+      0xC3 => Jp(self.opcode_u16()),
+      0xE9 => JpHL(),
+
+      0x18 => Jr(self.opcode_i8()),
+
       0xE0 => HiWrite(self.opcode_u8()),
       0xF0 => HiLoad(self.opcode_u8()),
       0xE2 => HiWriteReg(),
       0xF2 => HiLoadReg(),
+
+      0x08 => WriteMemSP(self.opcode_u16()),
+      0xE8 => AddSP(self.opcode_i8()),
+      0xF8 => LoadSPOffset(self.opcode_i8()),
+      0xF9 => SwapSPHL(),
+
+      0x09 | 0x19 | 0x29 | 0x39 => AddHL(reg16[((opcode & 0xf0) >> 4) as usize]),
 
       0x80 ... 0x87 => AddReg8(reg8[(opcode & 0x0f) as usize]),
       0x88 ... 0x8F => AddCarryReg8(reg8[(opcode & 0x0f - 8) as usize]),
@@ -390,11 +388,85 @@ impl CPU {
       0xB0 ... 0xB7 => OrReg8(reg8[(opcode & 0x0f) as usize]),
       0xB8 ... 0xBF => Compare(reg8[(opcode & 0x0f - 8) as usize]),
 
+      0xC6 => AddImm8(self.opcode_u8()),
+      0xD6 => SubImm8(self.opcode_u8()),
+      0xE6 => AndImm8(self.opcode_u8()),
+      0xF6 => OrImm8(self.opcode_u8()),
+      0xCE => AddCarryImm8(self.opcode_u8()),
+      0xDE => SubCarryImm8(self.opcode_u8()),
+      0xEE => XorImm8(self.opcode_u8()),
+      0xFE => CompareImm8(self.opcode_u8()),
+
       0x01 | 0x11 | 0x21 | 0x31 => LoadImm16(reg16[((opcode & 0xf0) >> 4) as usize], self.opcode_u16()),
-      0xC1 | 0xD1 | 0xE1 | 0xF1 => Pop(reg16[(((opcode & 0xf0) >> 4) - 12) as usize]),
+
+      0x0A | 0x1A => ReadA(reg16[((opcode & 0xf0) >> 4) as usize], InstrFlag::None),
+      0x2A => ReadA(Reg16::HL, InstrFlag::Inc), 
+      0x3A => ReadA(Reg16::HL, InstrFlag::Dec), 
+
       0x02 | 0x12 => WriteA(reg16[((opcode & 0xf0) >> 4) as usize], InstrFlag::None),
       0x22 => WriteA(Reg16::HL, InstrFlag::Inc), 
       0x32 => WriteA(Reg16::HL, InstrFlag::Dec), 
+
+      0xEA => WriteAImm16(self.opcode_u16()),
+      0xFA => ReadAImm16(self.opcode_u16()),
+
+      0xC1 | 0xD1 | 0xE1 | 0xF1 => Pop(reg16[(((opcode & 0xf0) >> 4) - 12) as usize]),
+      0xC5 | 0xD5 | 0xE5 | 0xF5 => Push(reg16[(((opcode & 0xf0) >> 4) - 12) as usize]),
+
+      0x40 ... 0x47 => LoadReg8(Reg8::B, reg8[(opcode & 0x0f) as usize]),
+      0x48 ... 0x4F => LoadReg8(Reg8::C, reg8[(opcode & 0x0f - 8) as usize]),
+      0x50 ... 0x57 => LoadReg8(Reg8::D, reg8[(opcode & 0x0f) as usize]),
+      0x58 ... 0x5F => LoadReg8(Reg8::E, reg8[(opcode & 0x0f - 8) as usize]),
+      0x60 ... 0x67 => LoadReg8(Reg8::H, reg8[(opcode & 0x0f) as usize]),
+      0x68 ... 0x6F => LoadReg8(Reg8::L, reg8[(opcode & 0x0f - 8) as usize]),
+      0x70 ... 0x75 | 0x77 => LoadReg8(Reg8::MemHL, reg8[(opcode & 0x0f) as usize]),
+      0x78 ... 0x7F => LoadReg8(Reg8::A, reg8[(opcode & 0x0f - 8) as usize]),
+
+      0x03 | 0x13 | 0x23 | 0x33 => IncReg16(reg16[((opcode & 0xf0) >> 4) as usize]),
+
+      0x0B | 0x1B | 0x2B | 0x3B => DecReg16(reg16[((opcode & 0xf0) >> 4) as usize]),
+
+      0x04 | 0x14 | 0x24 | 0x34 => IncReg8(reg8[(((opcode & 0xf0) >> 4) * 2) as usize]),
+      0x0C | 0x1C | 0x2C | 0x3C => IncReg8(reg8[(((opcode & 0xf0) >> 4) * 2 + 1) as usize]),
+
+      0x05 | 0x15 | 0x25 | 0x35 => DecReg8(reg8[(((opcode & 0xf0) >> 4) * 2) as usize]),
+      0x0D | 0x1D | 0x2D | 0x3D => DecReg8(reg8[(((opcode & 0xf0) >> 4) * 2 + 1) as usize]),
+
+      0x06 | 0x16 | 0x26 | 0x36 => LoadImm8(reg8[(((opcode & 0xf0) >> 4) * 2) as usize], self.opcode_u8()),
+      0x0E | 0x1E | 0x2E | 0x3E => LoadImm8(reg8[(((opcode & 0xf0) >> 4) * 2 + 1) as usize], self.opcode_u8()),
+
+      0x76 => Halt(),
+
+      0xF3 => DisableInterrupts(),
+      0xFB => EnableInterrupts(),
+
+      0x07 => Rlca(),
+      0x17 => Rla(),
+      0x27 => Daa(),
+      0x37 => SetCarryFlag(),
+      0x0F => Rrca(),
+      0x1F => Rra(),
+      0x2F => Complement(),
+      0x3F => ComplementCarry(),
+
+      0xC7 => Restart(0x00),
+      0xD7 => Restart(0x10),
+      0xE7 => Restart(0x20),
+      0xF7 => Restart(0x30),
+      0xCF => Restart(0x08),
+      0xDF => Restart(0x18),
+      0xEF => Restart(0x28),
+      0xFF => Restart(0x38),
+
+      0xCB => {
+        let next_opcode = self.opcode_u8();
+        match next_opcode {
+          // TODO: complete 0xCB table
+          _ => {
+            panic!("Unhandled 0xCB opcode {:?}", next_opcode);
+          }
+        }
+      },
       
       _ => {
         panic!("Unknown opcode {:?}", opcode);
